f9af681d9f78755942baee88e1600227cf34eb02
FreeAccount907 <61273086+FreeAccount907@users.noreply.github.com>
2020-02-20 11:30:31 +0100

    Städat
    Städat och lagt in externa källor

---
+++ questions.js
@@ -0,0 +1,35 @@
+var quizQuestions = [{
+  id: 1,
+  title: 'Question 1',
+  question: 'What is the answer to question 1?',
+  choices: ['A', 'B', 'C'],
+  correctAnswer: 1
+},
+{
+  id: 2,
+  title: 'Question 2',
+  question: 'What is the answer to question 2?',
+  choices: ['A', 'B', 'C'],
+  correctAnswer: 2
+},
+{
+  id: 3,
+  title: 'Question 3',
+  question: 'What is the answer to question 3?',
+  choices: ['A', 'B', 'C'],
+  correctAnswer: 3
+},
+{
+  id: 4,
+  title: 'Question 4',
+  question: 'What is the answer to question 3?',
+  choices: ['A', 'B', 'C'],
+  correctAnswer: 3
+},
+{
+  id: 5,
+  title: 'Question 5',
+  question: 'What is the answer to question 4?',
+  choices: ['A', 'B', 'C'],
+  correctAnswer: 1
+}];
