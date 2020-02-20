b20f0737c6c609cc8d41e191bf602e5dba42b241
lofersfanclub <lindahl.jonatan@gmail.com>
2020-02-20 15:51:12 +0100

    new

---
+++ gameplay.js
@@ -0,0 +1,24 @@
+var correctAnwers =[];
+var playerAnswers= [];
+
+var currentGame= false;
+
+document.getElementById("playButton").addEventListener("click", function () {
+       gameStart();
+});
+
+
+function gameStart(){
+var currentGame = true;
+    if(currentGame){
+        console.log("play game");
+        document.getElementById('questions').style.display = "block"
+        document.getElementById('scoreboard').style.display = "none"
+        loopAllQuestions();
+    }
+    else{
+
+    }
+
+}
+
