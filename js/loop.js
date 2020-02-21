439046415721d7045adada1e56f5c6b74b5171f2
FreeAccount907 <61273086+FreeAccount907@users.noreply.github.com>
2020-02-21 17:18:01 +0100

    Uppdaterat
    Lite rough scriptat med fungerande score osv.

--- js/loop.js
+++ js/loop.js
@@ -1,24 +1,100 @@
+var playerScore = 0;
+
+
 //Get number of questions
 var numQ = quizQuestions.length;
 
 //loop through all questions
 for (i = 0; i < numQ; i++){
 
-  //create question div
-  qCont = document.createElement('div');
-  qCont.setAttribute("id", "q" + quizQuestions[i]['id']);
-  qCont.setAttribute("class", "c" + quizQuestions[i]['category']);
+  //create modal outer
+  qModal1 = document.createElement('div');
+  qModal1.setAttribute("id","modal-q" + quizQuestions[i]['id'] );
+  qModal1.setAttribute("class", "modal fade");
+  qModal1.setAttribute("role","dialog");
+  qModal1.setAttribute("aria-labelledby","modalLabel-" + quizQuestions[i]['id']);
+  qModal1.setAttribute("aria-hidden","true");
 
-  //fill question div with question content
-  qCont.innerHTML = "<h3>" +quizQuestions[i]['title'] +"</h3>";
-  qCont.innerHTML += "<p>" +quizQuestions[i]['question'] +"</p>";
+  //create modal inner
+  qModal2 = document.createElement('div');
+  qModal2.setAttribute("class", "question-modal modal-dialog");
+  qModal2.setAttribute("role","document");
 
-  //add choices to question div
+  //create modal content
+  qModal3 = document.createElement('div');
+  qModal3.setAttribute("class", "modal-content c" + quizQuestions[i]['category']);
+
+  //create modal containers
+  qModalHeader = document.createElement('div');
+  qModalHeader.setAttribute("class", "modal-header");
+
+  qModalBody = document.createElement('div');
+  qModalBody.setAttribute("class", "modal-body");
+
+  qModalFooter = document.createElement('div');
+  qModalFooter.setAttribute("class", "modal-footer");
+
+  //fill modal with question title and content
+  qModalHeader.innerHTML = "<h4>" + quizQuestions[i]['title'] +"</h4";
+  qModalBody.innerHTML += '<p class="lead">' + quizQuestions[i]['question'] +"</p>";
+
+  //fill modal footer with options/choices
   for (n = 0; n < quizQuestions[i]['choices'].length; n++){
-    qCont.innerHTML += '<button id="'+ n +'">' + quizQuestions[i]['choices'][n] + '</button>';
+    //if (n == quizQuestions[i]['correctAnswer']){
+    //  qModalFooter.innerHTML += '<button class="btn btn-success" type="button" id="'+quizQuestions[i]['id']+"_"+ n +'">' + quizQuestions[i]['choices'][n] + '</button>';
+  //  }else{
+      qModalFooter.innerHTML += '<button id="q'+i+'a'+n+'"class="btn btn-light" type="button" onclick="validateChoice('+i+','+n+',q'+i+'a'+n+')">'+ quizQuestions[i]['choices'][n] +'</button>';
+  //  }
   }
 
-  //send div to container 
-  document.getElementById('questions').appendChild(qCont);
+  //send modals to container
+  document.getElementById('questions').appendChild(qModal1);
+  qModal1.appendChild(qModal2);
+  qModal2.appendChild(qModal3);
+  qModal3.appendChild(qModalHeader);
+  qModal3.appendChild(qModalBody);
+  qModal3.appendChild(qModalFooter);
 
+
+  //create buttons
+  qButton = document.createElement('button');
+  qButton.setAttribute("id","modal-b" + quizQuestions[i]['id'] );
+  qButton.setAttribute("class", "btn c"+ quizQuestions[i]['category']);
+  qButton.setAttribute("type","button");
+  qButton.setAttribute("data-toggle","modal");
+  qButton.setAttribute("data-target","#modal-q" + quizQuestions[i]['id'] );
+
+  //populate buttons
+  qButton.innerHTML = quizQuestions[i]['title'];
+
+  //send buttons to containers
+  document.getElementById('qButtons').appendChild(qButton);
 }
+
+function validateChoice(a,b,qid){
+  if (quizQuestions[a]['correctAnswer'] === b) {
+
+    //toggle classes to make btn green on correct (needs to update other buttons on click)
+    qid.classList.remove('btn-light');
+    qid.className += " btn-success";
+
+    //check if qustion has been answered
+    if (document.getElementById('modal-b'+a).disabled != true) {
+      //add score to scorevariable
+      playerScore ++;
+      //update score counter
+      document.getElementById('pscore').innerHTML = playerScore;
+    }
+  } else{
+
+    //toggle classes to make btn red on correct (needs to update other buttons on click)
+    qid.classList.remove('btn-light');
+    qid.classList += " btn-danger";
+  }
+
+//disable access to question when answered
+  document.getElementById('modal-b'+a).disabled = true;
+}
+
+
+//add function to end game (move to scoreboard) when all questions are disabled
--- js/questions.js
+++ js/questions.js
@@ -1,37 +1,37 @@
 var quizQuestions = [{
-  id: 1,
+  id: 0,
   category: 01,
   title: 'Question 1',
   question: 'What is the answer to question 1?',
   choices: ['A', 'B', 'C'],
-  correctAnswer: 1
+  correctAnswer: 0
 },
 {
-  id: 2,
+  id: 1,
   category: 02,
   title: 'Question 2',
   question: 'What is the answer to question 2?',
   choices: ['A', 'B', 'C'],
-  correctAnswer: 2
+  correctAnswer: 0
 },
 {
-  id: 3,
+  id: 2,
   category: 04,
   title: 'Question 3',
   question: 'What is the answer to question 3?',
   choices: ['A', 'B', 'C'],
-  correctAnswer: 3
+  correctAnswer: 0
 },
 {
-  id: 4,
+  id: 3,
   category: 01,
   title: 'Question 4',
   question: 'What is the answer to question 3?',
   choices: ['A', 'B', 'C'],
-  correctAnswer: 3
+  correctAnswer: 1
 },
 {
-  id: 5,
+  id: 4,
   category: 17,
   title: 'Question 5',
   question: 'What is the answer to question 4?',
